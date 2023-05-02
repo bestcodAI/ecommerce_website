e51ee924d2e7d6e41b40fbf864c27a7167d20c17
Chou Chamnan <109691277+bestcodAI@users.noreply.github.com>
2023-05-02 12:34:19 +0700

    update frontend

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,6 +9,9 @@ $route['translate_uri_dashes'] = FALSE;
 $route['login']           = 'admin/login';
 $route['register']        = 'admin/register';
 $route['dashboard']       = 'admin/dashboard';
+$route['dashboard2']      = 'admin/dashboard2';
+$route['dashboard3']      = 'admin/dashboard3';
+$route['wegit']           = 'admin/wegit';
 
 
 // frontend 
---
+++ application/controllers/Error.php
@@ -0,0 +1,9 @@
+<!-- /*<?php defined('BASEPATH') OR exit('No direct script access allowed');?> -->
+
+// class Error extends CI_Controller {
+
+//     public function error404()
+//     {
+//         $this->load->view('error/html/error404');
+//     }
+// } 
